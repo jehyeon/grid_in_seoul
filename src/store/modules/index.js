5f187ae81c56afd86ffd681900154c7b9c59ef15
jehye0nlee <v-lee.jehyeon@lionbridge.com>
2019-06-26 20:51:35 +0900

    Add pages redux store

---
+++ src/store/modules/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import pages from './pages';
+
+export default combineReducers({
+  pages,
+});
---
+++ src/store/modules/pages.js
@@ -0,0 +1,54 @@
+// Imports
+import data from '../MOCK_DATA.json';
+
+// Actions
+const SET_PAGES = 'pages/SET_PAGES';
+const SELECT_PAGE = 'pages/SELECT_PAGE';
+const NEXT_PAGE = 'pages/NEXT_PAGE';
+const PREVIOUS_PAGE = 'pages/PREVIOUS_PAGE';
+
+// Action Creators
+export const setPages = pages => ({ type: SET_PAGES, pages });
+export const selectPage = pageId => ({ type: SELECT_PAGE, pageId });
+export const nextPage = () => ({ type: NEXT_PAGE });
+export const previousPage = () => ({ type: PREVIOUS_PAGE });
+
+// Initial State
+const initialState = {
+  cursor: 0,
+  pages: data.pages,
+};
+
+// Reducer
+export default function pages(state = initialState, action) {
+  switch (action.type) {
+    case SET_PAGES:
+      return {
+        ...state,
+        pages: action.pages,
+      };
+    case SELECT_PAGE:
+      if (action.pageId >= 0 && action.pageId <= state.pages.length - 1) {
+        return {
+          ...state,
+          cursor: action.pageId,
+        };
+      } return state;
+    case NEXT_PAGE:
+      if (state.cursor < state.pages.length - 1) {
+        return {
+          ...state,
+          cursor: state.cursor + 1,
+        };
+      } return state;
+    case PREVIOUS_PAGE:
+      if (state.cursor > 0) {
+        return {
+          ...state,
+          cursor: state.cursor - 1,
+        };
+      } return state;
+    default:
+      return state;
+  }
+}
