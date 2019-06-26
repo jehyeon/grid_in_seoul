73b254cf22009fa83b46573d9e2b21950a96c352
jehye0nlee <v-lee.jehyeon@lionbridge.com>
2019-06-26 20:52:35 +0900

    Rename .jsx to .js and Try PagesContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import PagesContainer from './containers/PagesContainer';
 
 function App() {
   return (
     <div className="App">
-        Hello!
+      <PagesContainer />
     </div>
   );
 }
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/modules';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// eslint-disable-next-line no-underscore-dangle
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSIONS__();
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
