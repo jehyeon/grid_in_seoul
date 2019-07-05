719f4344bee9ee0b8d1d069d8765e1fe39a7f0c8
hyeon62 <hyeon.rot@gmail.com>
2019-07-05 16:53:26 +0900

    Delete temporary view for data validation and Coloring blocks'

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,8 +2,28 @@ import React from 'react';
 import propTypes from 'prop-types';
 import './Map.css';
 
-const Block = ({ name, id, value }) => (
-  <div className="block" id={id.toString()} name={name} value={value} />
+// Functions
+function getColor (values, item) {
+  if (values) {
+    const average = values.reduce((a, b) => a + b, 0) / (values.length - 1);
+    const css = {};
+
+    if (values[item] > average) {
+      const green = 255 - (values[item] - average) * 255 / (0.5 * average);
+      css.background = 'rgb(255,' + parseInt(green) + ',0)';
+    } else if (values[item] < average) {
+      const red = 255 - (average - values[item]) * 255 / (0.5 * average);
+      css.background = 'rgb(' + parseInt(red) + ',255,0)';
+    } else css.background = 'rgb(255,255,0)';
+
+    return css;
+  }
+}
+
+const Block = ({
+  name, id, value, backgroundColor,
+}) => (
+  <div style={backgroundColor} className="block" id={id.toString()} name={name} value={value} />
 );
 
 const BlockBox = ({ mapID, mapData, values }) => (
@@ -14,6 +34,7 @@ const BlockBox = ({ mapID, mapData, values }) => (
           id={item}
           name={mapID[item]}
           value={values[item]}
+          backgroundColor={getColor(values, item)}
         />
       ))
     ))}
@@ -24,6 +45,7 @@ Block.propTypes = {
   name: propTypes.string.isRequired,
   id: propTypes.number.isRequired,
   value: propTypes.number.isRequired,
+  backgroundColor: propTypes.objectOf(propTypes.string).isRequired,
 };
 
 BlockBox.propTypes = {
